90cdaedda9a76ed4c7e37d3a958c4c9f9f03fd55
Viloria, Reymart_HUMCOM1_IAA120212022 <97888543+Reymart-Viloria@users.noreply.github.com>
2022-04-29 21:45:33 +0800

    Add files via upload

---
+++ Finals folder/Exercise14(JS)/External/c.js
@@ -0,0 +1,6 @@
+console.log ("Viloria, Reymart N.");
+console.log ("BSIT");
+console.log ("First Year Second Semister");
+console.log ("HUMCOM 1");
+console.log ("IAA1 ");
+console.log ("Human Computer Interaction");
